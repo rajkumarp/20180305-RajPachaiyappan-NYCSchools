ed49405b10c21faf2ece6fed214705dda8795a02
Rajkumar Pachaiyappan <rajkumarp@rajkumars-macbook-pro.local>
2018-03-05 18:29:28 -0500

    Initial Commit

---
+++ 20180305-RajPachaiyappan-NYCSchools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180305-RajPachaiyappan-NYCSchools
+//
+//  Created by Rajkumar Pachaiyappan on 3/5/18.
+//  Copyright © 2018 Rajkumar Pachaiyappan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
